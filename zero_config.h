91c39b1affd818998dc83361dec5f62a225961e9
Dirk Mahoney <dirk@tcri.com.au>
2019-11-02 13:02:26 +1000

    Smaller CLI cmd line buffer

--- zero_config.h
+++ zero_config.h
@@ -35,7 +35,7 @@ namespace zero {
     const uint16_t CLI_STACK_BYTES = 512;
 
     // CLI command line buffer size
-    const uint16_t CLI_CMD_LINE_BUFFER_BYTES = 80;
+    const uint16_t CLI_CMD_LINE_BUFFER_BYTES = 40;
 
     // Maximum number of tokens on a CLI command line
     const uint16_t CLI_CMD_LINE_MAX_TOKENS = 16;
