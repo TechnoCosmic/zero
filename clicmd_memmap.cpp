8fca407677291d56e269df3fa6456a3b26c2e07e
Dirk Mahoney <dirk@tcri.com.au>
2019-11-18 10:26:43 +1000

    Added usage stats to the output

--- clicmd_memmap.cpp
+++ clicmd_memmap.cpp
@@ -21,12 +21,14 @@
 
 using namespace zero;
 
-const int PAGES_PER_ROW = 32;
+const int PAGES_PER_ROW = 64;
 
 
-static const PROGMEM char _totalAllocatableSram[] = "Total: ";
-static const PROGMEM char _usedSram[] = " Used: ";
-static const PROGMEM char _availableSram[] = "Avail: ";
+static const PROGMEM char _totalBytes[] = "Total: ";
+static const PROGMEM char _usedBytes[] = " Used: ";
+static const PROGMEM char _freeBytes[] = " Free: ";
+static const PROGMEM char _pcOpener[] = " (";
+static const PROGMEM char _pcCloser[] = "%)";
 
 static const char SYMBOL_ZEROPAGE = 'Z';
 static const char SYMBOL_GLOBAL = 'G';
@@ -78,6 +80,7 @@ static char getSymbolForAddress(const uint16_t address) {
     return SYMBOL_GLOBAL;
 }
 
+
 clicommand(memmap, (TextPipe* rx, TextPipe* tx, int argc, char* argv[]) {
     const int totalPages = memory::getTotalPages();
     const uint16_t ttlRam = memory::getTotalBytes();
@@ -92,7 +95,7 @@ clicommand(memmap, (TextPipe* rx, TextPipe* tx, int argc, char* argv[]) {
     for (uint16_t i = 0; i < 256; i += memory::getPageSizeBytes()) {
         *tx << 'Z';
     }
-    
+
     *tx << endl;
 
     // the rest of memory
@@ -100,7 +103,7 @@ clicommand(memmap, (TextPipe* rx, TextPipe* tx, int argc, char* argv[]) {
         const char symbol = getSymbolForAddress(curAddress);
         const Color col = getColorForSymbol(symbol);
 
-        if ((curAddress - 256) % (memory::getPageSizeBytes() * 64) == 0) {
+        if ((curAddress - 256) % (memory::getPageSizeBytes() * PAGES_PER_ROW) == 0) {
             *tx << setreverse(true);
             *tx << white << right << uppercase << hex << setfill('0') << setw(4) << (int32_t) curAddress;
             *tx << setreverse(false);
@@ -112,7 +115,7 @@ clicommand(memmap, (TextPipe* rx, TextPipe* tx, int argc, char* argv[]) {
 
         *tx << settextcolor(col) << (char) symbol;
 
-        if (((curAddress - 256) + memory::getPageSizeBytes()) % (memory::getPageSizeBytes() * 64) == 0) {
+        if (((curAddress - 256) + memory::getPageSizeBytes()) % (memory::getPageSizeBytes() * PAGES_PER_ROW) == 0) {
             *tx << endl;
         }
     }
@@ -124,6 +127,23 @@ clicommand(memmap, (TextPipe* rx, TextPipe* tx, int argc, char* argv[]) {
     *tx << settextcolor(getColorForSymbol(SYMBOL_GLOBAL)) << SYMBOL_GLOBAL << white << ": Globals" << endl;    
     *tx << settextcolor(getColorForSymbol(SYMBOL_USED)) << SYMBOL_USED << white << ": Allocated\t\t";    
     *tx << settextcolor(getColorForSymbol(SYMBOL_AVAIL)) << SYMBOL_AVAIL << white << ": Available" << endl;    
+    *tx << endl;
+
+    // usage information
+    const uint16_t sz = memory::getPageSizeBytes();
+    const uint32_t ttl = memory::getTotalBytes();
+    const uint16_t used = memory::getUsedBytes();
+    const uint16_t free = memory::getFreeBytes();
+
+    const uint32_t usedPc = ((uint32_t) used * 100ULL) / ttl;
+    const uint32_t freePc = ((uint32_t) free * 100ULL) / ttl;
+
+    *tx << PGM(_totalBytes) << setw(5) << setfill(' ') << right << ttl << endl;
+
+    *tx << PGM(_usedBytes) << setw(5) << setfill(' ') << right << used << PGM(_pcOpener) << setw(2) << setfill(' ') << right << (int32_t) usedPc << PGM(_pcCloser) << endl;
+    *tx << PGM(_freeBytes) << setw(5) << setfill(' ') << right << free << PGM(_pcOpener) << setw(2) << setfill(' ') << right << (int32_t) freePc << PGM(_pcCloser) << endl;
+
+    *tx << endl;
 
     return 0;
 });
