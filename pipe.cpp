202b2d1acf855d68dba9a851424152e7c89b26d2
Dirk Mahoney <dirk@tcri.com.au>
2019-11-18 16:33:29 +1000

    Tidy up

--- pipe.cpp
+++ pipe.cpp
@@ -90,7 +90,7 @@ bool Pipe::write(const uint8_t data, const bool allowBlock) {
 	}
 	
 	while (allowBlock && isFull()) {
-		Thread::block(ThreadState::TS_WAIT_WRITE, (uint32_t) this);
+		Thread::block(ThreadState::TS_PIPE_WRITE, (uint32_t) this);
 	}
 
 	// NOTE: There is potentially a race condition here if multiple
@@ -115,7 +115,7 @@ bool Pipe::write(const uint8_t data, const bool allowBlock) {
 			_length++;
 			// unblock any Thread that was blocked
 			// waiting for data to become available.
-			Thread::unblock(ThreadState::TS_WAIT_READ, (uint32_t) this);
+			Thread::unblock(ThreadState::TS_PIPE_READ, (uint32_t) this);
 		}
 	}
 	
@@ -150,7 +150,7 @@ bool Pipe::read(uint8_t* data, const bool allowBlock) {
 	}
 	
 	while (allowBlock && isEmpty()) {
-		Thread::block(ThreadState::TS_WAIT_READ, (uint32_t) this);
+		Thread::block(ThreadState::TS_PIPE_READ, (uint32_t) this);
 	}
 
 	// NOTE: There is potentially a race condition here if multiple
@@ -178,7 +178,7 @@ bool Pipe::read(uint8_t* data, const bool allowBlock) {
 				_start = 0;
 			}
 
-			Thread::unblock(ThreadState::TS_WAIT_WRITE, (uint32_t) this);
+			Thread::unblock(ThreadState::TS_PIPE_WRITE, (uint32_t) this);
 			return true;
 		}
 	}
