56985447422252f5482195674c2042cf2526d9c4
Dirk Mahoney <dirk@tcri.com.au>
2020-01-11 11:07:10 +1000

    Whitespace

--- pagemanager.cpp
+++ pagemanager.cpp
@@ -17,24 +17,24 @@ using namespace zero;
 
 
 // bit-field manipulation macros
-#define BF_BYTE(b) ((int)(b) >> 3)
-#define BF_BIT(b) ((int)(b) & 0b111)
+#define BF_BYTE(b)          ((int)(b) >> 3)
+#define BF_BIT(b)           ((int)(b) & 0b111)
 
-#define BF_SET(bf,b) bf[BF_BYTE(b)] |= (1 << BF_BIT(b))
-#define BF_CLR(bf,b) bf[BF_BYTE(b)] &= ~(1 << BF_BIT(b))
-#define BF_TST(bf,b) (bf[BF_BYTE(b)] & (1 << BF_BIT(b)))
+#define BF_SET(bf,b)        bf[BF_BYTE(b)] |= (1 << BF_BIT(b))
+#define BF_CLR(bf,b)        bf[BF_BYTE(b)] &= ~(1 << BF_BIT(b))
+#define BF_TST(bf,b)        (bf[BF_BYTE(b)] & (1 << BF_BIT(b)))
 
 // friendly names for the bit-field manipulators
-#define IS_PAGE_AVAILABLE(b) (!BF_TST(_memoryMap,b))
-#define MARK_AS_USED(b) (BF_SET(_memoryMap,b))
-#define MARK_AS_FREE(b) (BF_CLR(_memoryMap,b))
+#define IS_PAGE_AVAIL(b)    (!BF_TST(_memoryMap,b))
+#define MARK_AS_USED(b)     (BF_SET(_memoryMap,b))
+#define MARK_AS_FREE(b)     (BF_CLR(_memoryMap,b))
 
 
 // Has the page been marked as used, or is it free?
 template <uint16_t PAGE_COUNT>
 bool PageManager<PAGE_COUNT>::isPageAvailable(const uint16_t pageNumber)
 {
-    return IS_PAGE_AVAILABLE(pageNumber);
+    return IS_PAGE_AVAIL(pageNumber);
 }
 
 
