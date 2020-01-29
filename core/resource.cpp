e65422939b6a95167fcde5daddc85f40cef68156
Dirk Mahoney <dirk@tcri.com.au>
2020-01-29 15:54:35 +1000

    Basic resource management added

---
+++ core/resource.cpp
@@ -0,0 +1,41 @@
+//
+// zero - pre-emptive multitasking kernel for AVR
+//
+// Techno Cosmic Research Institute    Dirk Mahoney           dirk@tcri.com.au
+// Catchpole Robotics                  Christian Catchpole    christian@catchpole.net
+//
+
+
+#include <util/atomic.h>
+#include "resource.h"
+
+
+using namespace zero;
+
+
+namespace {
+    uint16_t _resourceMap = 0UL;
+}
+
+
+bool resource::obtain(const ResourceId id)
+{
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        uint16_t m = 1L << (uint16_t) id;
+
+        if (!(_resourceMap & m)) {
+            _resourceMap |= m;
+            return true;
+        }
+
+        return false;
+    }
+}
+
+
+void resource::release(const ResourceId id)
+{
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        _resourceMap &= ~(1L << (uint16_t) id);
+    }
+}
---
+++ core/resource.h
@@ -0,0 +1,43 @@
+//
+// zero - pre-emptive multitasking kernel for AVR
+//
+// Techno Cosmic Research Institute    Dirk Mahoney           dirk@tcri.com.au
+// Catchpole Robotics                  Christian Catchpole    christian@catchpole.net
+//
+
+
+#ifndef TCRI_ZERO_RESOURCE_H
+#define TCRI_ZERO_RESOURCE_H
+
+
+#include <stdint.h>
+
+
+namespace zero {
+
+    namespace resource {
+
+        enum class ResourceId {
+            Timer0 = 0,
+            Timer1,
+            Timer2,
+            UsartRx0,
+            UsartRx1,
+            UsartRx2,
+            UsartRx3,
+            UsartTx0,
+            UsartTx1,
+            UsartTx2,
+            UsartTx3,
+            Spi,
+        };
+
+        bool obtain(const ResourceId r);
+        void release(const ResourceId r);
+
+    };
+
+}
+
+
+#endif
