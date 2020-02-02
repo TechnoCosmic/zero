7ab778eedbe5b5ecb99bea80ace2ae414663bf4d
Dirk Mahoney <dirk@tcri.com.au>
2020-02-03 07:16:48 +1000

    Slight refactoring regarding pins

--- drivers/gpio.h
+++ drivers/gpio.h
@@ -17,57 +17,61 @@
 #include <avr/io.h>
 
 
-#define PIN(port,pin) (((port) << 3) | ((pin) & 7))
-
-
 namespace zero {
 
 
     typedef uint32_t PinField;
 
 
+    // Maps a port number and pin number to a simple integer
+    constexpr uint32_t pinPort(const uint8_t port, const uint8_t pin)
+    {
+        return ((uint32_t) port << 3) | ((uint32_t) pin & 7);
+    }
+
+
     #ifdef PORTA
-        const PinField PF_PINA0 = (1L << PIN(0, 0));
-        const PinField PF_PINA1 = (1L << PIN(0, 1));
-        const PinField PF_PINA2 = (1L << PIN(0, 2));
-        const PinField PF_PINA3 = (1L << PIN(0, 3));
-        const PinField PF_PINA4 = (1L << PIN(0, 4));
-        const PinField PF_PINA5 = (1L << PIN(0, 5));
-        const PinField PF_PINA6 = (1L << PIN(0, 6));
-        const PinField PF_PINA7 = (1L << PIN(0, 7));
+        const PinField ZERO_PINA0 = (1L << pinPort(0, 0));
+        const PinField ZERO_PINA1 = (1L << pinPort(0, 1));
+        const PinField ZERO_PINA2 = (1L << pinPort(0, 2));
+        const PinField ZERO_PINA3 = (1L << pinPort(0, 3));
+        const PinField ZERO_PINA4 = (1L << pinPort(0, 4));
+        const PinField ZERO_PINA5 = (1L << pinPort(0, 5));
+        const PinField ZERO_PINA6 = (1L << pinPort(0, 6));
+        const PinField ZERO_PINA7 = (1L << pinPort(0, 7));
     #endif
 
     #ifdef PORTB
-        const PinField PF_PINB0 = (1L << PIN(1, 0));
-        const PinField PF_PINB1 = (1L << PIN(1, 1));
-        const PinField PF_PINB2 = (1L << PIN(1, 2));
-        const PinField PF_PINB3 = (1L << PIN(1, 3));
-        const PinField PF_PINB4 = (1L << PIN(1, 4));
-        const PinField PF_PINB5 = (1L << PIN(1, 5));
-        const PinField PF_PINB6 = (1L << PIN(1, 6));
-        const PinField PF_PINB7 = (1L << PIN(1, 7));
+        const PinField ZERO_PINB0 = (1L << pinPort(1, 0));
+        const PinField ZERO_PINB1 = (1L << pinPort(1, 1));
+        const PinField ZERO_PINB2 = (1L << pinPort(1, 2));
+        const PinField ZERO_PINB3 = (1L << pinPort(1, 3));
+        const PinField ZERO_PINB4 = (1L << pinPort(1, 4));
+        const PinField ZERO_PINB5 = (1L << pinPort(1, 5));
+        const PinField ZERO_PINB6 = (1L << pinPort(1, 6));
+        const PinField ZERO_PINB7 = (1L << pinPort(1, 7));
     #endif
 
     #ifdef PORTC
-        const PinField PF_PINC0 = (1L << PIN(2, 0));
-        const PinField PF_PINC1 = (1L << PIN(2, 1));
-        const PinField PF_PINC2 = (1L << PIN(2, 2));
-        const PinField PF_PINC3 = (1L << PIN(2, 3));
-        const PinField PF_PINC4 = (1L << PIN(2, 4));
-        const PinField PF_PINC5 = (1L << PIN(2, 5));
-        const PinField PF_PINC6 = (1L << PIN(2, 6));
-        const PinField PF_PINC7 = (1L << PIN(2, 7));
+        const PinField ZERO_PINC0 = (1L << pinPort(2, 0));
+        const PinField ZERO_PINC1 = (1L << pinPort(2, 1));
+        const PinField ZERO_PINC2 = (1L << pinPort(2, 2));
+        const PinField ZERO_PINC3 = (1L << pinPort(2, 3));
+        const PinField ZERO_PINC4 = (1L << pinPort(2, 4));
+        const PinField ZERO_PINC5 = (1L << pinPort(2, 5));
+        const PinField ZERO_PINC6 = (1L << pinPort(2, 6));
+        const PinField ZERO_PINC7 = (1L << pinPort(2, 7));
     #endif
 
     #ifdef PORTD
-        const PinField PF_PIND0 = (1L << PIN(3, 0));
-        const PinField PF_PIND1 = (1L << PIN(3, 1));
-        const PinField PF_PIND2 = (1L << PIN(3, 2));
-        const PinField PF_PIND3 = (1L << PIN(3, 3));
-        const PinField PF_PIND4 = (1L << PIN(3, 4));
-        const PinField PF_PIND5 = (1L << PIN(3, 5));
-        const PinField PF_PIND6 = (1L << PIN(3, 6));
-        const PinField PF_PIND7 = (1L << PIN(3, 7));
+        const PinField ZERO_PIND0 = (1L << pinPort(3, 0));
+        const PinField ZERO_PIND1 = (1L << pinPort(3, 1));
+        const PinField ZERO_PIND2 = (1L << pinPort(3, 2));
+        const PinField ZERO_PIND3 = (1L << pinPort(3, 3));
+        const PinField ZERO_PIND4 = (1L << pinPort(3, 4));
+        const PinField ZERO_PIND5 = (1L << pinPort(3, 5));
+        const PinField ZERO_PIND6 = (1L << pinPort(3, 6));
+        const PinField ZERO_PIND7 = (1L << pinPort(3, 7));
     #endif
 
 
