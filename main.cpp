fbe621e8d857e9aa3007a2c2916439d1e5f653a6
Dirk Mahoney <dirk@tcri.com.au>
2020-10-12 18:07:52 +1000

    Better example main()

--- main.cpp
+++ main.cpp
@@ -7,11 +7,26 @@
 
 #include "thread.h"
 
+
 using namespace zero;
+
+
+int myThreadMain()
+{
+    // put your thread code here
+
+    while( true ) {
+        // empty
+    }
+
+    // and if you exit, be sure to return something
+    return 0;
+}
+
 
 // Set things up, spawn your initial Threads. Get your party started here.
 // But let main() exit - only once main() returns will the kernel finish initializing.
 int main()
 {
-    ;
+    new Thread( PSTR( "myThreadName" ), 256, myThreadMain );
 }
